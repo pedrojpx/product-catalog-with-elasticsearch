62e5d5d5d4b58fdd009a745b4c909fb6f046246d
pedrojpx <pjpxavier@gmail.com>
2021-09-27 23:48:21 -0300

    configurações iniciais do elasticsearch

---
+++ product-catalog/src/test/java/com/springcloudapp/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudapp.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
