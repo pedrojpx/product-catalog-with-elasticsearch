4ecfc5267f49915545ae96ce710a0516c5098fa1
pedrojpx <pjpxavier@gmail.com>
2021-10-14 02:06:15 -0300

    configurando um projeto de gateway

---
+++ gateway/src/test/java/com/springcloudapp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
